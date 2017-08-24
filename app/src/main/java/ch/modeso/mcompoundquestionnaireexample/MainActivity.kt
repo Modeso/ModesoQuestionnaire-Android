f32eca19c23e2c08ca1ad11babc032de0965f633
Hazem Emad <hazem.emad@modeso.ch>
2017-08-24 16:43:29 +0200

    example improvement

--- app/src/main/java/ch/modeso/mcompoundquestionnaireexample/MainActivity.kt
+++ app/src/main/java/ch/modeso/mcompoundquestionnaireexample/MainActivity.kt
@@ -11,14 +11,18 @@ import java.util.*
 class MainActivity : AppCompatActivity(), CardInteractionCallbacks {
 
     val title = ArrayList<UserModel>()
+    val randText = listOf<String>("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus semper, nulla at bibendum auctor, lacus velit rutrum leo",
+            "At lobortis velit nisl non turpis. Praesent feugiat dui at massa consequat, sit amet pulvinar risus aliquet. ",
+            "Aliquam nec viverra mauris. Nulla libero justo, euismod at dignissim et, elementum eu felis. Vestibulum mollis nisl in volutpat molestie.",
+            "Sed consectetur, metus fermentum posuere semper, nunc nisl luctus lectus, sit amet placerat ipsum leo ut ante. Donec consequat diam nibh,",
+            "In consequat massa pellentesque ut. Sed suscipit et enim tincidunt tempor. Mauris nec efficitur lacus, in consectetur felis. ")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar!!.elevation = 0f
-        val size = 50
-        for (i in 0..size - 1) {
-            title.add(UserModel("Hello $i", "additional info $i",QuestionnaireCardView.CardStatus.NONE))
+        for (i in 0..19) {
+            title.add(UserModel(randText[i % 5], "additional info $i", QuestionnaireCardView.CardStatus.NONE))
         }
         mcompound_questionnaire.updateList(title.toMutableList())
         mcompound_questionnaire.cardInteractionCallBacks = this
